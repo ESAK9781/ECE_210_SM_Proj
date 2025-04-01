a9b48d838be83a111afdc6ef5f0e8a8bda90f0cd
Elijah Sakamoto <esak.code.projects@gmail.com>
2025-04-01 11:34:30 -0600

    testing complete

--- main.c
+++ main.c
@@ -10,16 +10,30 @@ int main(int argC, char * argV[]) {
 
     if (TEST_MACHINE) {
         printf("Enter initial s2s1: ");
-        char vals[3];
-        fgets(vals, 3, stdin);
+        char vals[10];
+        fgets(vals, 10, stdin);
         StateMachine * sm = createMachine(vals[0] == '1', vals[1] == '1');
 
 
 
         printf("Success! Loaded machine.\n");
         printf("Initial "); printMachine(sm);
+        printf("q to exit\n");
 
+        while (true) {
+            printf("a2a1b: ");
+            fgets(vals, 10, stdin);
+            if (vals[0] == 'q') break;
 
+            bool a2 = (vals[0] == '1') ? true : false;
+            bool a1 = (vals[1] == '1') ? true : false;
+            bool b = (vals[2] == '1') ? true : false;
+
+            printf("\tPrev "); printMachine(sm);
+            StateOut out = eval_up(sm, inp(a2, a1, b));
+            printf("\t"); printMachine(sm);
+            printf("\tOutpt: %c%c\n", (out.o2) ? '1' : '0', (out.o1) ? '1' : '0');
+        }
 
 
         free(sm);
