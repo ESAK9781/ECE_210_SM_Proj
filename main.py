058a7a26a232685e3fd03dcc53e1ef8a354291b7
Elijah Sakamoto <esak.code.projects@gmail.com>
2025-04-01 08:30:46 -0600

    states library finished

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+WRITE_PROGRAM = True
+
 # Generate the table
 inpts = ['a2', 'a1', 'b', 's2', 's1']
 outpt = ['q2', 'q1', 'o2', 'o1']
@@ -7,7 +9,7 @@ def conv_bin(bin_l):
     cur_place = 1
     val = 0
     for i in range(len(bin_l)):
-        ind = len((bin_l - 1) - i)
+        ind = (len(bin_l) - 1) - i
         if (bin_l[ind] == 1):
             val += cur_place
         cur_place *= 2
@@ -17,6 +19,46 @@ def conv_bin(bin_l):
 
 def computeOut(inp):
     a = [inp[0], inp[1]]
+    b = inp[2]
+    s = [inp[3], inp[4]]
+    
+    if ((s[0] and s[1]) or (a[0] and a[1])):
+        return ['#', '#', '#', '#']
+    
+    a = conv_bin(a)
+    s = conv_bin(s)
+    
+    # a += 1 # just because of the way we are encoding a and s
+    # s += 1
+    
+    q = 0 # changed later
+    o = [inp[3], inp[4]] # clone the current state -- moore machine
+    
+    if (b == 0):
+        s -= a
+    else:
+        s += a
+        
+    # s -= 1 # re-encode into the 0 indexed states
+
+    loop_val = 3
+    s += loop_val * 10 # loop everything
+    s %= loop_val
+    
+    if s == 0:
+        q = [0, 0]
+    elif s == 1:
+        q = [0, 1]
+    elif s == 2:
+        q = [1, 0]
+    else:
+        print("ERROR, ERROR")
+        
+        
+    out = [q[0], q[1], o[0], o[1]]
+    return out
+    
+    
 
 
 def cpy(lst):
@@ -50,13 +92,60 @@ def test(cur_inp, rem_inp):
     
     
 test([], inpts)
+for line in table:
+    str_out = ""
+    for val in line:
+        str_out += str(val) + ", "
+    print(str_out)
+        
+# Generate the C boolean algebra using sum of products
+
+def par(exp): # put an expression in parentheses
+    return "(" + exp + ")"
 
 
+c_code = ""
+for i in range(len(outpt)):
+    out_name = outpt[i]
+    out_col = i + len(inpts)
+    out_exp = "\t" + out_name + " = "
+    first_1 = True
+    for row in table:
+        if (row[out_col] == 1):
+            first_2 = True
+            cur_exp = ""
+            for j in range(len(inpts)):
+                if (not first_2):
+                    cur_exp += " && "
+                else:
+                    first_2 = False
+                if (row[j] == 0):
+                    cur_exp += par("!" + inpts[j])
+                else:
+                    cur_exp += inpts[j]
+            if not first_1:
+                out_exp += " || " + par(cur_exp)
+            else:
+                first_1 = False
+                out_exp += par(cur_exp)
+    c_code += out_exp + ";\n"
 
-table = [
-    ['a2', 'a1', 'b', ]   
-]
+c_program = ""
+with open("./states.h", 'r') as file:
+    c_program = file.read()
 
-outputs = ''
+if (WRITE_PROGRAM):
+    parts = c_program.split("// SMLOGIC")
+    if (len(parts) != 3):
+        print("ERROR: Could not find flags in program")
+        exit(-1)
+    parts[1] = "\n" + c_code
+    c_program = parts[0] + "// SMLOGIC" + parts[1] + "// SMLOGIC" + parts[2]
+    
+    with open("./states.h", 'w') as file:
+        file.write(c_program)
+        print("SUCCESS: C program written to file")
+    
 
-print("Hello World");
+
+        
--- states.h
+++ states.h
@@ -1,10 +1,89 @@
 #ifndef STATES_H
 #define STATES_H
 
+#include <stdbool.h>
+#include <stdlib.h>
 
 typedef struct StateM_Struct {
-    
+    bool s2;
+    bool s1;
 } StateMachine;
 
+typedef struct StateM_In_Struct {
+    bool a2;
+    bool a1;
+    bool b;
+} StateIn;
+
+typedef struct StateM_Out_Struct {
+    bool q2;
+    bool q1;
+    bool o2;
+    bool o1;
+} StateOut;
+
+/**
+ * @brief evaluates a state machine given input in
+ * @return output
+ */
+StateOut eval(StateMachine * sm, StateIn in) {
+    StateOut out;
+    bool a2 = in.a2;
+    bool a1 = in.a1;
+    bool b = in.b;
+    bool s2 = sm->s2;
+    bool s1 = sm->s1;
+
+    bool q2, q1, o2, o1;
+
+// SMLOGIC
+	q2 = ((!a2) && (!a1) && (!b) && s2 && (!s1)) || ((!a2) && (!a1) && b && s2 && (!s1)) || ((!a2) && a1 && (!b) && (!s2) && (!s1)) || ((!a2) && a1 && b && (!s2) && s1) || (a2 && (!a1) && (!b) && (!s2) && s1) || (a2 && (!a1) && b && (!s2) && (!s1));
+	q1 = ((!a2) && (!a1) && (!b) && (!s2) && s1) || ((!a2) && (!a1) && b && (!s2) && s1) || ((!a2) && a1 && (!b) && s2 && (!s1)) || ((!a2) && a1 && b && (!s2) && (!s1)) || (a2 && (!a1) && (!b) && (!s2) && (!s1)) || (a2 && (!a1) && b && s2 && (!s1));
+	o2 = ((!a2) && (!a1) && (!b) && s2 && (!s1)) || ((!a2) && (!a1) && b && s2 && (!s1)) || ((!a2) && a1 && (!b) && s2 && (!s1)) || ((!a2) && a1 && b && s2 && (!s1)) || (a2 && (!a1) && (!b) && s2 && (!s1)) || (a2 && (!a1) && b && s2 && (!s1));
+	o1 = ((!a2) && (!a1) && (!b) && (!s2) && s1) || ((!a2) && (!a1) && b && (!s2) && s1) || ((!a2) && a1 && (!b) && (!s2) && s1) || ((!a2) && a1 && b && (!s2) && s1) || (a2 && (!a1) && (!b) && (!s2) && s1) || (a2 && (!a1) && b && (!s2) && s1);
+// SMLOGIC
+
+    out.q2 = q2;
+    out.q1 = q1;
+    out.o2 = o2;
+    out.o1 = o1;
+
+    return out;
+}
+
+/**
+ * @brief evaluate the state machine with given input, also update state
+ * @return output
+ */
+
+ StateOut eval_up(StateMachine * sm, StateIn in) {
+    StateOut out = eval(sm, in);
+    sm->s1 = out.q1;
+    sm->s2 = out.q2;
+    return out;
+ }
+
+ /**
+  * @brief StateInput constructor
+  * @return state input
+  */
+ StateIn inp(bool a2, bool a1, bool b) {
+    StateIn out;
+    out.a1 = a1;
+    out.a2 = a2;
+    out.b = b;
+    return out;
+ }
+
+ /**
+  * @brief StateMachine constructor
+  * @return pointer to the new state machine
+  */
+
+  StateMachine * createMachine(bool s2, bool s1) {
+    StateMachine * out = (StateMachine *) malloc(sizeof(StateMachine));
+    out->s2 = s2;
+    out->s1 = s1;
+  }
 
 #endif
